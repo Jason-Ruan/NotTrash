276966609f64987c173f06813d21dfc2d3549532
Jason Ruan <jasonruan@pursuit.org>
2019-12-18 18:17:50 -0500

    Adds ItemImageCollectionViewCell file

---
+++ NotTrash/Views/ItemImageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemImageCollectionViewCell.swift
+//  NotTrash
+//
+//  Created by Jason Ruan on 12/18/19.
+//  Copyright © 2019 albert coelho oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ItemImageCollectionViewCell: UICollectionViewCell {
+    
+}
