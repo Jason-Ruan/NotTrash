b829900527be659262b05c4603d7148fb4d78129
karyRmartinez <karymartinez@pursuit.org>
2019-12-19 11:48:02 -0500

    progress bar

---
+++ NotTrash/ViewControllers/ProfileVC/CircularProgressView.swift
@@ -0,0 +1,21 @@
+//
+//  CircularProgressView.swift
+//  NotTrash
+//
+//  Created by Kary Martinez on 12/19/19.
+//  Copyright © 2019 albert coelho oliveira. All rights reserved.
+//
+
+import UIKit
+
+class CircularProgressView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
